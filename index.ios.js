dbd47c61f981cd2154adce8e7a7fdc73e101289e
shashankm <shashank.shashi2@gmail.com>
2016-07-28 12:30:36 +0530

    Progress bar initial commit

---
+++ index.ios.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import ProgressBarClassic from './ProgressBarClassic'
+
+class ProgressBar extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ProgressBarClassic
+            progress={40}
+            />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('ProgressBar', () => ProgressBar);
